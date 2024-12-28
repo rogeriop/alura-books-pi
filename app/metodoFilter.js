2b6272d38d0cb36b382304562c6b5ece85d6d85c
rogeriop <pinheiroxrogerio@gmail.com>
2024-12-28 18:31:14 -0300

    03. Filter: aplicando diferentes filtros

---
+++ app/metodoFilter.js
@@ -0,0 +1,15 @@
+
+//const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront');
+
+//btnFiltrarLivrosDeFront.addEventListener('click', filtrarLivros);
+
+const botoes = document.querySelectorAll('.btn');
+
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));
+
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id);
+    const categoria = elementoBtn.value
+    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria);
+    exibirOsLivrosNaTela(livrosFiltrados);
+}
