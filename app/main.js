290768984ca96f90a9570656699bd8601596a9fe
rogeriop <pinheiroxrogerio@gmail.com>
2024-12-28 09:40:58 -0300

    02. Map: novo array modificado

--- app/main.js
+++ app/main.js
@@ -2,30 +2,12 @@ let livros = [];
 const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 getBuscarLivrosDaAPI();
 
-const elementoParaInserirLivros = document.getElementById('livros');
 
 
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI);
     livros = await res.json();
-    exibirOsLivrosNaTela(livros);
+    let livrosComDesconto = aplicarDesconto(livros);
+    exibirOsLivrosNaTela(livrosComDesconto);
 }
 
-function exibirOsLivrosNaTela(listaDeLivros) {
-    listaDeLivros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `
-    <div class="livro">
-      <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-      <h2 class="livro__titulo">
-        ${livro.titulo}
-      </h2>
-      <p class="livro__descricao">${livro.autor}</p>
-      <p class="livro__preco" id="preco">R$${livro.preco}29,90</p>
-      <div class="tags">
-        <span class="tag">${livro.categoria}</span>
-      </div>
-    </div>
-        
-        `
-    });
-}
